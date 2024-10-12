1a7ba50d7be34ff9b819b232f858f81be34df0da
ahmedhegazy211977 <38991237+ahmedhegazy211977@users.noreply.github.com>
2024-10-12 13:26:02 +0300

    Delete leajlak_delivery directory

--- leajlak_delivery/controllers/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import main,api_controller
--- leajlak_delivery/models/delivery_carrier.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-
-class DeliveryCarrier(models.Model):
-   _inherit = "delivery.carrier"
-   is_for_you_delivery = fields.Boolean('Leajlak Delivery')
