db9c94f236112a4309a5b400a06ab3da9c225d7d
ahmedhegazy211977 <38991237+ahmedhegazy211977@users.noreply.github.com>
2024-10-17 11:49:25 +0300

    Delete leajlak_delivery directory

--- leajlak_delivery/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_config_settings ,delivery_carrier ,stock_picking,sale_order,res_country,res_country_state
--- leajlak_delivery/models/res_country_state.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import models,fields
-
-
-class ResCountryState(models.Model):
-    _inherit = 'res.country.state'
-    show_on_website = fields.Boolean('Show on website')
